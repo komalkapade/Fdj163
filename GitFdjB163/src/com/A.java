8bf2f9d6d71e8fdfee052b0fbe7920615e2a42e0
51194_Dipak <dipakpatil1996@gmail.com>
2024-05-13 15:32:54 +0530

    By Komal

--- GitFdjB163/src/com/A.java
+++ GitFdjB163/src/com/A.java
@@ -1,8 +1,9 @@
+package com;
 
 public class A {
-
 	
 	public static void main(String[] args) {
-		System.out.println("This is 1 git project");
+		System.out.println("This is git 1 project.");
 	}
+
 }
