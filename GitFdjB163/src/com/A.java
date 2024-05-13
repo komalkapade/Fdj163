9626e6693c777d77444e7ceade15431b7df2b282
komalkapade <komalk14698@gmail.com>
2024-05-13 15:56:02 +0530

    Update A.java

--- GitFdjB163/src/com/A.java
+++ GitFdjB163/src/com/A.java
@@ -6,6 +6,11 @@ public class A {
 	{
 		System.out.println("M1 Method");
 	}
+
+	public void m2()
+	{
+		System.out.println("m2 Method");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("This is git 1 project.");
